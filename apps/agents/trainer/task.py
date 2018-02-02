b94b9a12597bda21c3f242c164b345772f6d0a13
cwbeitel <me@cwbeitel-github.com>
2018-02-02 11:18:31 -0800

    reinforcement learning with tensorflow/agents
    Status:
    * short run functions without issue, see notebook, long run in progress
    * Needs E2E test
    * need GCP project permissions to modify service account roles or have someone do so to be able to test run tboard stuff

---
+++ apps/agents/trainer/task.py
@@ -0,0 +1,314 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Provides an entrypoint for the training task."""
+
+from __future__ import absolute_import, division, print_function
+
+import argparse
+import datetime
+import logging
+import os
+import pprint
+
+import pip
+import tensorflow as tf
+from google.cloud import storage
+
+import agents
+import pybullet_envs  # To make AntBulletEnv-v0 available.
+
+flags = tf.app.flags
+
+flags.DEFINE_string("run_mode", "train",
+                    "Run mode, one of [train, render, train_and_render].")
+flags.DEFINE_string("logdir", '/tmp/test',
+                    "The base directory in which to write logs and "
+                    "checkpoints.")
+flags.DEFINE_string("hparam_set_id", "pybullet_kuka_ff",
+                    "The name of the config object to be used to parameterize "
+                    "the run.")
+flags.DEFINE_string("run_base_tag",
+                    datetime.datetime.now().strftime('%Y%m%dT%H%M%S'),
+                    "Base tag to prepend to logs dir folder name. Defaults "
+                    "to timestamp.")
+flags.DEFINE_boolean("env_processes", True,
+                     "Step environments in separate processes to circumvent "
+                     "the GIL.")
+flags.DEFINE_boolean("sync_replicas", False,
+                     "Use the sync_replicas (synchronized replicas) mode, "
+                     "wherein the parameter updates from workers are "
+                     "aggregated before applied to avoid stale gradients.")
+flags.DEFINE_integer("num_gpus", 0,
+                     "Total number of gpus for each machine."
+                     "If you don't use GPU, please set it to '0'")
+flags.DEFINE_integer("save_checkpoint_secs", 600,
+                     "Number of seconds between checkpoint save.")
+flags.DEFINE_boolean("use_monitored_training_session", True,
+                     "Whether to use tf.train.MonitoredTrainingSession to "
+                     "manage the training session. If not, use "
+                     "tf.train.Supervisor.")
+
+flags.DEFINE_boolean("log_device_placement", False,
+                     "Whether to output logs listing the devices on which "
+                     "variables are placed.")
+flags.DEFINE_boolean("debug", True,
+                     "Run in debug mode.")
+flags.DEFINE_boolean("dump_dependency_versions", False,
+                     "Run in debug mode.")
+
+# Render
+flags.DEFINE_integer("render_secs", 600,
+                     "Number of seconds between triggering render jobs.")
+flags.DEFINE_string("render_out_dir", None,
+                    "The path to which to copy generated renders.")
+
+# Algorithm
+flags.DEFINE_string("algorithm", "agents.algorithms.PPO",
+                    "The name of the algorithm to use.")
+flags.DEFINE_integer("num_agents", 30,
+                     "The number of agents to use.")
+flags.DEFINE_integer("eval_episodes", 25,
+                     "The number of eval episodes to use.")
+flags.DEFINE_string("env", "AntBulletEnv-v0",
+                    "The gym / bullet simulation environment to use.")
+flags.DEFINE_integer("max_length", 1000,
+                     "The maximum length of an episode.")
+flags.DEFINE_integer("steps", 10000000,
+                     "The number of steps.")
+
+# Network
+flags.DEFINE_string("network", "agents.scripts.networks.feed_forward_gaussian",
+                    "The registered network name to use for policy and value.")
+# TODO: Make it possible to pass policy and value layers via hparam config.
+# flags.DEFINE_string("policy_layers", "200,100",
+#                      "A comma-separates list of the size of a series of layers "
+#                      "to comprise the policy network.")
+# flags.DEFINE_string("value_layers", "200,100",
+#                      "A comma-separates list of the size of a series of layers "
+#                      "to comprise the value network.")
+flags.DEFINE_float("init_mean_factor", 0.1,
+                   "")
+flags.DEFINE_float("init_std", 0.35,
+                   "")
+
+# Optimization
+flags.DEFINE_float("learning_rate", 1e-4,
+                   "The learning rate of the optimizer.")
+flags.DEFINE_string("optimizer", "tensorflow.train.AdamOptimizer",
+                    "The import path of the optimizer to use.")
+flags.DEFINE_integer("update_epochs", 25,
+                     "The number of update epochs.")
+flags.DEFINE_integer("update_every", 30,
+                     "The update frequency.")
+# flags.DEFINE_boolean("optimizer_pre_initialize", True,
+#                      "Whether to pre-initialize the optimizer.")
+
+# Losses
+flags.DEFINE_float("discount", 0.995,
+                   "The discount.")
+flags.DEFINE_float("kl_target", 1e-2,
+                   "the KL target.")
+flags.DEFINE_integer("kl_cutoff_factor", 2,
+                     "The KL cutoff factor.")
+flags.DEFINE_integer("kl_cutoff_coef", 1000,
+                     "The KL cutoff coefficient.")
+flags.DEFINE_integer("kl_init_penalty", 1,
+                     "The initial KL penalty?.")
+
+FLAGS = flags.FLAGS
+
+
+def hparams_base():
+  """Base hparams tf/Agents PPO """
+
+  # General
+  algorithm = agents.algorithms.PPO
+  num_agents = 30
+  eval_episodes = 30
+  use_gpu = False
+
+  # Environment
+  env = 'KukaBulletEnv-v0'
+  normalize_ranges = True
+  max_length = 1000
+
+  # Network
+  network = agents.scripts.networks.feed_forward_gaussian
+  weight_summaries = dict(
+      all=r'.*', policy=r'.*/policy/.*', value=r'.*/value/.*')
+  policy_layers = 200, 100
+  value_layers = 200, 100
+  init_output_factor = 0.1
+  init_logstd = -1
+  init_std = 0.35
+
+  # Optimization
+  update_every = 60
+  update_epochs = 25
+  optimizer = tf.train.AdamOptimizer
+  learning_rate = 1e-4
+  steps = 3e7  # 30M
+
+  # Losses
+  discount = 0.995
+  kl_target = 1e-2
+  kl_cutoff_factor = 2
+  kl_cutoff_coef = 1000
+  kl_init_penalty = 1
+
+  return locals()
+
+
+def _object_import_from_string(name):
+
+  components = name.split('.')
+  mod = __import__(components[0])
+  for comp in components[1:]:
+    mod = getattr(mod, comp)
+  return mod
+
+
+def _realize_import_attrs(d, filter):
+  for k, v in d.items():
+    if k in filter:
+      # try:
+      imported = _object_import_from_string(v)
+      # except ImportError as e:
+      #     msg = ("Failed to realize import path %s." % v)
+      #     raise e
+      d[k] = imported
+  return d
+
+
+def _get_agents_configuration(hparam_set_name, log_dir=None, is_chief=False):
+  """Load hyperparameter config."""
+  try:
+    # Try to resume training.
+    hparams = agents.scripts.utility.load_config(log_dir)
+  except IOError:
+
+    hparams = hparams_base()
+
+    # --------
+    # Experiment extending base hparams with FLAGS and dynamic import of
+    # network and algorithm.
+    for k, v in FLAGS.__dict__['__flags'].items():
+      hparams[k] = v
+    hparams = _realize_import_attrs(
+        hparams, ["network", "algorithm", "optimizer"])
+    # --------
+
+    hparams = agents.tools.AttrDict(hparams)
+
+    if is_chief:
+      # Write the hyperparameters for this run to a config YAML for posteriority
+      hparams = agents.scripts.utility.save_config(hparams, log_dir)
+
+  pprint.pprint(hparams)
+  return hparams
+
+
+def _dump_dependency_versions():
+  tf.logging.info("Tensorflow version: %s", tf.__version__)
+  tf.logging.info("Tensorflow git version: %s", tf.__git_version__)
+
+  pprint.pprint(sorted(["%s==%s" % (i.key, i.version)
+                        for i in pip.get_installed_distributions()]))
+
+  if FLAGS.run_mode == 'render':
+    os.system("ffmpeg -version")
+
+
+def gcs_upload(local_dir, gcs_out_dir):
+  """Upload the contents of a local directory to a specific GCS path.
+
+  Args:
+    local_dir (str): The local directory containing files to upload.
+    gcs_out_dir (str): The target Google Cloud Storage directory path.
+
+  Raises:
+    ValueError: If `gcs_out_dir` does not start with "gs://".
+
+  """
+
+  # Get a list of all files in the local_dir
+  local_files = [f for f in os.listdir(
+      local_dir) if os.path.isfile(os.path.join(local_dir, f))]
+  tf.logging.info("Preparing local files for upload:\n %s" % local_files)
+
+  # Initialize the GCS API client
+  storage_client = storage.Client()
+
+  # Raise an error if the target directory cannot be a GCS path
+  if not gcs_out_dir.startswith("gs://"):
+    raise ValueError(
+        "gcs_upload expected gcs_out_dir argument to start with gs://, saw %s" % gcs_out_dir)
+
+  # TODO: Detect and handle case where a GCS path has been provdied
+  # corresponding to a bucket that does not exist or for which the user does
+  # not have permissions.
+
+  # Obtain the bucket path from the total path
+  bucket_path = gcs_out_dir.split('/')[2]
+  bucket = storage_client.get_bucket(bucket_path)
+
+  # Construct a target upload path that excludes the initial gs://bucket-name
+  blob_base_path = '/'.join(gcs_out_dir.split('/')[3:])
+
+  # For each local file *name* in the list of local file names
+  for local_filename in local_files:
+
+    # Construct the target and local *paths*
+    blob_path = os.path.join(blob_base_path, local_filename)
+    blob = bucket.blob(blob_path)
+    local_file_path = os.path.join(local_dir, local_filename)
+
+    # Perform the upload operation
+    blob.upload_from_filename(local_file_path)
+
+
+def main(unused_argv):
+  """Run training."""
+  tf.logging.set_verbosity(tf.logging.INFO)
+
+  if FLAGS.dump_dependency_versions:
+    _dump_dependency_versions()
+
+  if FLAGS.debug:
+    tf.logging.set_verbosity(tf.logging.DEBUG)
+
+  run_config = tf.contrib.learn.RunConfig()
+
+  log_dir = FLAGS.logdir
+
+  agents_config = _get_agents_configuration(
+      FLAGS.hparam_set_id, log_dir, run_config.is_chief)
+
+  if FLAGS.run_mode == 'train':
+    if run_config.is_chief:
+      for score in agents.scripts.train.train(agents_config, env_processes=True):
+        logging.info('Score {}.'.format(score))
+  if FLAGS.run_mode == 'render':
+    render_tmp_dir = "/tmp/agents-render"
+    os.system('mkdir %s' % render_tmp_dir)
+    agents.scripts.visualize.visualize(
+        logdir=FLAGS.logdir, outdir=render_tmp_dir, num_agents=1, num_episodes=1,
+        checkpoint=None, env_processes=True)
+    render_out_dir = FLAGS.render_out_dir
+    if render_out_dir is None:
+      render_out_dir = os.path.join(FLAGS.logdir, "render")
+    gcs_upload(render_tmp_dir, render_out_dir)
+
+
+if __name__ == '__main__':
+  tf.app.run()
---
+++ ml-app/components/agents.jsonnet
@@ -0,0 +1,106 @@
+local params = std.extVar("__ksonnet/params").components["agents"];
+local k = import 'k.libsonnet';
+local deployment = k.extensions.v1beta1.deployment;
+local container = deployment.mixin.spec.template.spec.containersType;
+local podTemplate = k.extensions.v1beta1.podTemplate;
+
+local tfJob = import 'kubeflow/tf-job/tf-job.libsonnet';
+
+local name = params.name;
+local namespace = params.namespace;
+local num_gpus = params.num_gpus;
+local hparam_set_id = params.hparam_set_id;
+local gcp_project = params.gcp_project;
+local jobTag = params.job_tag;
+local logDir = params.log_dir;
+local image = params.image;
+local imageGpu = params.image_gpu;
+local numCpu = params.num_cpu;
+local dumpDependencyVersions = params.dump_dependency_versions;
+local runMode = params.run_mode;
+local logdir = params.logdir;
+local hparamSetID = params.hparam_set_id;
+local runBaseTag = params.run_base_tag;
+local syncReplicas = params.sync_replicas;
+local numGpus = params.num_gpus;
+local algorithm = params.algorithm;
+local numAgents = params.num_agents;
+local evalEpisodes = params.eval_episodes;
+local env = params.env;
+local maxLength = params.max_length;
+local steps = params.steps;
+local network = params.network;
+local initMeanFactor = params.init_mean_factor;
+local initLogstd = params.init_logstd;
+local learningRate = params.learning_rate;
+local optimizer = params.optimizer;
+local updateEpochs = params.update_epochs;
+local updateEvery = params.update_every;
+local discount = params.discount;
+local klTarget = params.kl_target;
+local klCutoffFactor = params.kl_cutoff_factor;
+local klCutoffCoef = params.kl_cutoff_coef;
+local klInitPenalty = params.kl_init_penalty;
+
+local renderSecs = params.render_secs;
+
+local args = [
+  "--run_mode=" + runMode,
+  "--logdir=" + logdir,
+  "--hparam_set_id=" + hparamSetID,
+  "--run_base_tag=" + runBaseTag,
+  "--sync_replicas=" + syncReplicas,
+  "--num_gpus=" + numGpus,
+  "--algorithm=" + algorithm,
+  "--num_agents=" + numAgents,
+  "--eval_episodes=" + evalEpisodes,
+  "--env=" + env,
+  "--max_length=" + maxLength,
+  "--steps=" + steps,
+  "--network=" + network,
+  //"--policy_layers=" + policyLayers,
+  //"--value_layers=" + valueLayers,
+  "--init_mean_factor=" + initMeanFactor,
+  "--init_logstd=" + initLogstd,
+  "--learning_rate=" + learningRate,
+  "--optimizer=" + optimizer,
+  "--update_epochs=" + updateEpochs,
+  "--update_every=" + updateEvery,
+  "--discount=" + discount,
+  "--kl_target=" + klTarget,
+  "--kl_cutoff_factor=" + klCutoffFactor,
+  "--kl_cutoff_coef=" + klCutoffCoef,
+  "--kl_init_penalty=" + klInitPenalty,
+  "--dump_dependency_versions=" + dumpDependencyVersions,
+  "--render_secs=" + renderSecs,
+];
+
+local workerSpec = if numGpus > 0 then
+  	tfJob.parts.tfJobReplica("MASTER", 1, args, imageGpu, numGpus)
+  	else
+  	tfJob.parts.tfJobReplica("MASTER", 1, args, image);
+
+local replicas = std.map(function(s)
+  s + {
+    template+: {
+      spec+:  {
+        containers: [
+          s.template.spec.containers[0] + {
+            resources: {
+              limits: {
+                cpu: numCpu
+              },
+              requests: {
+                cpu: numCpu
+              }
+            },
+          },
+        ],
+      },
+    },
+  },
+  std.prune([workerSpec]));
+
+local job = tfJob.parts.tfJob(name, namespace, replicas);
+
+std.prune(k.core.v1.list.new([job]))
