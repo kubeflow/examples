ff94b10f5db6b1e2879be863be2cf594100faf6e
Ankush Agarwal <agwl@google.com>
2018-04-05 15:39:52 -0700

    pylint issues

--- github_issue_summarization/docker/flask_web/app.py
+++ github_issue_summarization/docker/flask_web/app.py
@@ -3,15 +3,23 @@ Simple app that parses predictions from a trained model and displays them.
 """
 
 import os
-import requests
 import re
+
+import requests
 from flask import Flask, json, render_template, request
+
 APP = Flask(__name__)
 GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 
+
 def get_issue_body(issue_url):
-  issue_url = re.sub('.*github.com/', 'https://api.github.com/repos/', issue_url)
-  return requests.get(issue_url, headers={'Authorization': 'token {}'.format(GITHUB_TOKEN)}).json()['body']
+  issue_url = re.sub('.*github.com/', 'https://api.github.com/repos/',
+                     issue_url)
+  return requests.get(
+    issue_url, headers={
+      'Authorization': 'token {}'.format(GITHUB_TOKEN)
+    }).json()['body']
+
 
 @APP.route("/")
 def index():
@@ -20,6 +28,7 @@ def index():
   Placeholder, does nothing.
   """
   return render_template("index.html")
+
 
 @APP.route("/summary", methods=['GET', 'POST'])
 def summary():
@@ -31,27 +40,22 @@ def summary():
   if request.method == 'POST':
     issue_text = request.form["issue_text"]
     issue_url = request.form["issue_url"]
-    if len(issue_url) > 0:
+    if issue_url:
       issue_text = get_issue_body(issue_url)
     url = "http://ambassador/seldon/issue-summarization/api/v0.1/predictions"
     headers = {'content-type': 'application/json'}
-    json_data = {
-      "data" : {
-        "ndarray" : [[issue_text]]
-      }
-    }
+    json_data = {"data": {"ndarray": [[issue_text]]}}
 
-    response = requests.post(url=url,
-                             headers=headers,
-                             data=json.dumps(json_data))
+    response = requests.post(
+      url=url, headers=headers, data=json.dumps(json_data))
 
     response_json = json.loads(response.text)
     issue_summary = response_json["data"]["ndarray"][0][0]
 
-    return render_template("issue_summary.html",
-                           issue_text=issue_text,
-                           issue_summary=issue_summary)
+    return render_template(
+      "issue_summary.html", issue_text=issue_text, issue_summary=issue_summary)
   return ('', 204)
+
 
 if __name__ == '__main__':
   APP.run(debug=True, host='0.0.0.0', port=80)
