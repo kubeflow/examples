21506ffc5138016f794f90ecb80ed379d2047010
Sanyam Kapoor <activatedgeek@users.noreply.github.com>
2018-06-20 15:34:05 -0700

    Python package for indexing and serving the index (#150)
    * Add a utility python package for indexing and serving the index
    
    * Add CLI arguments, conditional GCS download
    
    * Complete skeleton CLIs for serving and index creation
    
    * Fix lint issues

---
+++ code_search/indexing_server/nmslib_flask/search_engine.py
@@ -0,0 +1,46 @@
+import nmslib
+import numpy as np
+
+
+class CodeSearchEngine:
+  """This is a utility class which takes an nmslib
+  index file and a data file to return data from"""
+  def __init__(self, index_file: str, data_file: str):
+    self._index_file = index_file
+    self._data_file = data_file
+
+    self.index = CodeSearchEngine.nmslib_init()
+    self.index.loadIndex(index_file)
+
+    # TODO: load the reverse-index map for actual code data
+    # self.data_map =
+
+  def embed(self, query_str):
+    # TODO load trained model and embed input strings
+    raise NotImplementedError
+
+  def query(self, query_str: str, k=2):
+    embedding = self.embed(query_str)
+    idxs, dists = self.index.knnQuery(embedding, k=k)
+
+    # TODO(sanyamkapoor): initialize data map and return
+    # list of dicts
+    # [
+    #     {'src': self.data_map[idx], 'dist': dist}
+    #     for idx, dist in zip(idxs, dists)
+    # ]
+    return idxs, dists
+
+  @staticmethod
+  def nmslib_init():
+    """Initializes an nmslib index object"""
+    index = nmslib.init(method='hnsw', space='cosinesimil')
+    return index
+
+  @staticmethod
+  def create_index(data: np.array, save_path: str):
+    """Add numpy data to the index and save to path"""
+    index = CodeSearchEngine.nmslib_init()
+    index.addDataPointBatch(data)
+    index.createIndex({'post': 2}, print_progress=True)
+    index.saveIndex(save_path)
