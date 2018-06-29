68ea175b3a5b31cf2383cf2a9406f91c159150be
Sanyam Kapoor <sanyamkapoor@google.com>
2018-06-29 12:19:38 -0700

    Add similarity transformer body

--- code_search/app/code_search/t2t/similarity_transformer.py
+++ code_search/app/code_search/t2t/similarity_transformer.py
@@ -1,7 +1,9 @@
 import os
+import tensorflow as tf
 from tensor2tensor.utils import t2t_model
 from tensor2tensor.utils import registry
 from tensor2tensor.data_generators import text_problems
+from tensor2tensor.models import transformer
 
 
 @registry.register_model
@@ -12,11 +14,37 @@ class SimilarityTransformer(t2t_model.T2TModel):
   This class defines the model to compute similarity scores between functions and
   docstrings
   """
+  def __init__(self, *args, **kwargs):
+    super(SimilarityTransformer, self).__init__(*args, **kwargs)
+
+    self.code_transformer = transformer.TransformerEncoder(*args, **kwargs)
+    self.docstring_transformer = transformer.TransformerEncoder(*args, **kwargs)
 
   def body(self, features):
-    # TODO: need to fill this with Transformer encoder/decoder
-    # and loss calculation
-    raise NotImplementedError
+    """The body of this model takes in features
+    of code and docstring pairs, embeds & computes a
+    similarity probability and computes the loss as
+    standard cross entropy loss
+    """
+    string_embedding = self.code_transformer(features['inputs'])
+
+    loss = None
+    if 'targets' in features:
+      code_embedding = self.docstring_transformer(features['targets'])
+      cosine_dist = tf.losses.cosine_distance(tf.nn.l2_normalize(string_embedding, axis=1),
+                                              tf.nn.l2_normalize(code_embedding, axis=1),
+                                              axis=1, reduction=tf.losses.Reduction.NONE)
+
+      # TODO: need to do negative sampling, so this won't be all ones anymore
+      labels = tf.one_hot(tf.ones(features['targets'].shape[0], tf.int32), 2)
+      logits = tf.concat([cosine_dist, 1 - cosine_dist], axis=1)
+
+      loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=logits)
+
+    if loss:
+      return string_embedding, loss
+
+    return string_embedding
 
 
 @registry.register_problem
@@ -33,7 +61,7 @@ class GithubFunctionDocstring(text_problems.Text2TextProblem):
   def generate_samples(self, data_dir, _tmp_dir, dataset_split):  #pylint: disable=no-self-use
     """This method returns the generator to return {"inputs": [text], "targets": [text]} dict"""
 
-    functions_file_path = os.path.join(data_dir, '{}.function'.format(dataset_split))
     docstrings_file_path = os.path.join(data_dir, '{}.docstring'.format(dataset_split))
+    functions_file_path = os.path.join(data_dir, '{}.function'.format(dataset_split))
 
-    return text_problems.text2text_txt_iterator(functions_file_path, docstrings_file_path)
+    return text_problems.text2text_txt_iterator(docstrings_file_path, functions_file_path)
