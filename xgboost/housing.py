88384955c673be76621d91f77e6da029ba839f44
Puneith Kaul <puneith@google.com>
2018-07-04 18:02:31 -0700

    added code refactoring

--- xgboost/housing.py
+++ xgboost/housing.py
@@ -1,4 +1,5 @@
 import argparse
+import joblib
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import Imputer
@@ -6,14 +7,17 @@ from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error
 
-def read_input(file_name):
+def read_input(file_name, test_size=0.25):
+    """Read input data and split it into train and test."""
     data = pd.read_csv(file_name[0])
     data.dropna(axis=0, subset=['SalePrice'], inplace=True)
 
     y = data.SalePrice
     X = data.drop(['SalePrice'], axis=1).select_dtypes(exclude=['object'])
 
-    train_X, test_X, train_y, test_y = train_test_split(X.values, y.values, test_size=0.25)
+    train_X, test_X, train_y, test_y = train_test_split(X.values,
+                                                        y.values,
+                                                        test_size=test_size)
     
     imputer = Imputer()
     train_X = imputer.fit_transform(train_X)
@@ -27,6 +31,7 @@ def train_model(train_X,
                 test_y,
                 n_estimators,
                 learning_rate):
+    """Train the model using XGBRegressor."""
     model = XGBRegressor(n_estimators=n_estimators,
                          learning_rate=learning_rate)
 
@@ -37,9 +42,25 @@ def train_model(train_X,
     return model
 
 def eval_model(model, test_X, test_y):
+    """Evaluate the model performance."""
     predictions = model.predict(test_X)
     print("Mean Absolute Error : " + str(mean_absolute_error(predictions, test_y)))
 
+def save_model(model, model_file):
+    """Save XGBoost model for serving."""
+    joblib.dump(model, model_file)
+
+def main(args):
+    (train_X, train_y), (test_X, test_y) = read_input(args.train_input)
+    model = train_model(train_X,
+                        train_y,
+                        test_X,
+                        test_y,
+                        args.n_estimators,
+                        args.learning_rate)
+
+    eval_model(model, test_X, test_y)
+    save_model(model, args.model_file)
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
@@ -60,15 +81,16 @@ if __name__=='__main__':
             help='Learning rate for the model',
             default=0.1
     )
+    parser.add_argument(
+            '--model-file',
+            help='Model file location for XGBoost',
+            required=True
+    )
+    parser.add_argument(
+            '--test-size',
+            help='Fraction of training data to be reserved for test',
+            default=0.25
+    )
 
     args = parser.parse_args()
-    
-    (train_X, train_y), (test_X, test_y) = read_input(args.train_input)
-    model = train_model(train_X,
-                        train_y,
-                        test_X,
-                        test_y,
-                        args.n_estimators,
-                        args.learning_rate)
-
-    eval_model(model, test_X, test_y)
+    main(args)
