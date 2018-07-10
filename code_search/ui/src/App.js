9d56819ff1d435fe6a72da53178e807940042009
Sanyam Kapoor <sanyamkapoor@google.com>
2018-07-10 15:36:53 -0700

    Add component for code search results, dummy values for now

--- code_search/ui/src/App.js
+++ code_search/ui/src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { MuiThemeProvider } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+
 import blueTheme from './theme';
 import logo from './logo.svg';
 import './App.css';
-import SearchIcon from '@material-ui/icons/Search';
+import CodeSample from './CodeSample';
 
 class App extends Component {
   render() {
@@ -25,6 +27,7 @@ class App extends Component {
                 }}
               />
             </div>
+            <div className="Search-Wrapper">
               <Button
                 variant="contained"
                 color="primary"
@@ -32,6 +35,36 @@ class App extends Component {
               >
                 <SearchIcon/>Search Code
               </Button>
+            </div>
+            <div className="Search-Results">
+              <h2 className="Search-Results-Title">Search Results</h2>
+              {/** TODO: for illustrative purposes only  **/}
+              <CodeSample
+                nwo="activatedgeek/torchrl"
+                path="torchrl/agents/random_gym_agent.py"
+                lineno={19}
+                function_string={`
+def act(self, obs):
+  return [[self.action_space.sample()] for _ in range(len(obs))]
+                `}
+              />
+              <hr />
+              <CodeSample
+                nwo="activatedgeek/torchrl"
+                path="torchrl/policies/epsilon_greedy.py"
+                lineno={4}
+                function_string={`
+distribution = np.ones((len(choices), action_size),
+                       dtype=np.float32) * eps / action_size
+distribution[np.arange(len(choices)), choices] += 1.0 - eps
+actions = np.array([
+    np.random.choice(np.arange(action_size), p=dist)
+    for dist in distribution
+])
+return np.expand_dims(actions, axis=1)
+                `}
+              />
+            </div>
         </div>
       </MuiThemeProvider>
     );
