fd2e7509906d47186a9d9f32c1068d1123091cd5
Sanyam Kapoor <activatedgeek@users.noreply.github.com>
2018-08-01 13:37:41 -0700

    Fix T2T memory problem (#205)
    * Update T2T problems to workaround memory limitations
    
    * Add max_samples_for_vocab to prevent memory overflow
    
    * Fix a base URL to download data from, sweet spot for max samples
    
    * Convert class variables to class properties
    
    * Fix lint errors
    
    * Use Python2/3 compatible code for StringIO
    
    * Fix lint errors
    
    * Fix source data files format
    
    * Move to Text2TextProblem instead of TranslateProblem
    
    * Update details for num_shards and T2T problem dataset

--- code_search/src/code_search/t2t/function_docstring.py
+++ code_search/src/code_search/t2t/function_docstring.py
@@ -1,32 +1,59 @@
 """Github function/text similatrity problems."""
 import csv
-import os
-from cStringIO import StringIO
+from six import StringIO
 from tensor2tensor.data_generators import generator_utils
-from tensor2tensor.data_generators import translate
+from tensor2tensor.data_generators import text_problems
 from tensor2tensor.utils import metrics
 from tensor2tensor.utils import registry
-
-
-##
-# These URLs are only for fallback purposes in case the specified
-# `data_dir` does not contain the data. However, note that the data
-# files must have the same naming pattern.
-# TODO: The memory is exploding, need to fix this.
-#
-_DATA_BASE_URL = 'gs://kubeflow-examples/t2t-code-search/data'
-_GITHUB_FUNCTION_DOCSTRING_FILES = [
-    'pairs-0000{}-of-00010.csv'.format(i)
-    for i in range(1)
-]
+import tensorflow as tf
 
 
 @registry.register_problem
-class GithubFunctionDocstring(translate.TranslateProblem):
-  # pylint: disable=abstract-method
+class GithubFunctionDocstring(text_problems.Text2TextProblem):
+  """Function and Docstring similarity Problem.
 
-  """This class defines the problem of finding similarity between Python
-  function and docstring"""
+  This problem contains the data consisting of function
+  and docstring pairs as CSV files. The files are structured
+  such that they contain two columns without headers containing
+  the docstring tokens and function tokens. The delimiter is
+  ",".
+  """
+  @property
+  def pair_files_list(self):
+    """Return URL and file names.
+
+    This format is a convention across the Tensor2Tensor (T2T)
+    codebase. It should be noted that the file names are currently
+    hardcoded. This is to preserve the semantics of a T2T problem.
+    In case a change of these values is desired, one must subclass
+    and override this property.
+
+    # TODO(sanyamkapoor): Manually separate train/eval data set.
+
+    Returns:
+      A list of the format,
+        [
+          [
+            "STRING",
+            ("STRING", "STRING", ...)
+          ],
+          ...
+        ]
+      Each element is a list of size 2 where the first represents
+      the source URL and the next is an n-tuple of file names.
+
+      In this case, the tuple is of size 1 because the URL points
+      to a file itself.
+    """
+    base_url = "gs://kubeflow-examples/t2t-code-search/raw_data"
+
+    return [
+        [
+            "{}/func-doc-pairs-000{:02}-of-00100.csv".format(base_url, i),
+            ("func-doc-pairs-000{:02}-of-00100.csv".format(i),)
+        ]
+        for i in range(100)
+    ]
 
   @property
   def is_generate_per_split(self):
@@ -36,30 +63,37 @@ class GithubFunctionDocstring(translate.TranslateProblem):
   def approx_vocab_size(self):
     return 2**13
 
-  def source_data_files(self, dataset_split):  # pylint: disable=no-self-use,unused-argument
-    # TODO(sanyamkapoor): separate train/eval data set.
-    return _GITHUB_FUNCTION_DOCSTRING_FILES
+  @property
+  def max_samples_for_vocab(self):
+    # FIXME(sanyamkapoor): This exists to handle memory explosion.
+    return int(3.5e5)
 
-  def generate_samples(self, data_dir, tmp_dir, dataset_split):  # pylint: disable=no-self-use,unused-argument
-    """Returns a generator to return {"inputs": [text], "targets": [text]}.
+  def generate_samples(self, _data_dir, tmp_dir, _dataset_split):
+    """A generator to return data samples.Returns the data generator to return.
 
-    If the `data_dir` is a GCS path, all data is downloaded to the
-    `tmp_dir`.
+
+    Args:
+      data_dir: A string representing the data directory.
+      tmp_dir: A string representing the temporary directory and is
+              used to download files if not already available.
+      dataset_split: Train, Test or Eval.
+
+    Yields:
+      Each element yielded is of a Python dict of the form
+        {"inputs": "STRING", "targets": "STRING"}
     """
-
-    download_dir = tmp_dir if data_dir.startswith('gs://') else data_dir
-    uri_base = data_dir if data_dir.startswith('gs://') else _DATA_BASE_URL
-    pair_csv_files = [
-        generator_utils.maybe_download(download_dir, filename, os.path.join(uri_base, filename))
-        for filename in self.source_data_files(dataset_split)
+    csv_files = [
+        generator_utils.maybe_download(tmp_dir, file_list[0], uri)
+        for uri, file_list in self.pair_files_list
     ]
 
-    for pairs_file in pair_csv_files:
-      with open(pairs_file, 'r') as csv_file:
+    for pairs_file in csv_files:
+      tf.logging.debug("Reading {}".format(pairs_file))
+      with open(pairs_file, "r") as csv_file:
         for line in csv_file:
-          reader = csv.reader(StringIO(line), delimiter=',')
-          function_tokens, docstring_tokens = next(reader)[-2:]  # pylint: disable=stop-iteration-return
-          yield {'inputs': docstring_tokens, 'targets': function_tokens}
+          reader = csv.reader(StringIO(line))
+          for docstring_tokens, function_tokens in reader:
+            yield {"inputs": docstring_tokens, "targets": function_tokens}
 
   def eval_metrics(self):  # pylint: disable=no-self-use
     return [
