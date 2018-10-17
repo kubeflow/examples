3d1bc5adca050a2136586e3876e863bc69bc7cd3
David Sabater <davidsabater@google.com>
2018-10-17 22:30:40 +0100

    Fix lint errors

--- pytorch_mnist/serving/seldon-wrapper/mnistddpserving.py
+++ pytorch_mnist/serving/seldon-wrapper/mnistddpserving.py
@@ -16,50 +16,50 @@ limitations under the License.
 '''
 
 import torch
-import torch.utils.data
-import torch.utils.data.distributed
 import torch.nn as nn
 import torch.nn.functional as f
+import torch.utils.data
+import torch.utils.data.distributed
 from torchvision import transforms
 
 
 class Net(nn.Module):
-    """ Network architecture. """
+  """ Network architecture. """
 
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+  def __init__(self):
+    super(Net, self).__init__()
+    self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+    self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+    self.conv2_drop = nn.Dropout2d()
+    self.fc1 = nn.Linear(320, 50)
+    self.fc2 = nn.Linear(50, 10)
 
-    def forward(self, x):
-        x = f.relu(f.max_pool2d(self.conv1(x), 2))
-        x = f.relu(f.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = f.relu(self.fc1(x))
-        x = f.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return f.log_softmax(x)
+  def forward(self, x):
+    x = f.relu(f.max_pool2d(self.conv1(x), 2))
+    x = f.relu(f.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+    x = x.view(-1, 320)
+    x = f.relu(self.fc1(x))
+    x = f.dropout(x, training=self.training)
+    x = self.fc2(x)
+    return f.log_softmax(x)
 
 
 class mnistddpserving(object):
-    def __init__(self):
-        self.class_names = ["class:{}".format(str(i)) for i in range(10)]
-        self.model = Net()
-        # TODO parametrise path to load model
-        self.model.load_state_dict(torch.load("/mnt/kubeflow-fs/pytorch/model/model.dat",
-                                              map_location='cpu'))
-        # Ensure the model is in eval/inference mode
-        self.model.eval()
+  def __init__(self):
+    self.class_names = ["class:{}".format(str(i)) for i in range(10)]
+    self.model = Net()
+    # TODO parametrise path to load model
+    self.model.load_state_dict(torch.load("/mnt/kubeflow-fs/pytorch/model/model.dat",
+                                          map_location='cpu'))
+    # Ensure the model is in eval/inference mode
+    self.model.eval()
 
-    def predict(self, X, feature_names):
-        tensor = torch.from_numpy(X).view(-1, 28, 28)
-        t = transforms.Normalize((0.1307,), (0.3081,))
-        tensor_norm = t(tensor)
-        tensor_norm = tensor_norm.unsqueeze(0)
-        out = self.model(tensor_norm.float())
-        predictions = torch.nn.functional.softmax(out)
-        print(predictions)
-        return predictions.detach().numpy()
+  def predict(self, X, feature_names):
+    tensor = torch.from_numpy(X).view(-1, 28, 28)
+    t = transforms.Normalize((0.1307,), (0.3081,))
+    tensor_norm = t(tensor)
+    tensor_norm = tensor_norm.unsqueeze(0)
+    out = self.model(tensor_norm.float())
+    predictions = torch.nn.functional.softmax(out)
+    print(predictions)
+    return predictions.detach().numpy()
--- pytorch_mnist/web-ui/flask_server.py
+++ pytorch_mnist/web-ui/flask_server.py
@@ -15,8 +15,8 @@ limitations under the License.
 '''
 
 import os
-from threading import Timer
 import uuid
+from threading import Timer
 
 from flask import Flask, render_template, request
 from mnist_client import get_prediction, random_mnist
@@ -27,55 +27,55 @@ app = Flask(__name__)
 # handle requests to the server
 @app.route("/")
 def main():
-    # get url parameters for HTML template
-    name_arg = request.args.get('name', 'mnist-classifier')
-    addr_arg = request.args.get('addr', 'localhost')
-    port_arg = request.args.get('port', '8080')
-    args = {"name": name_arg, "addr": addr_arg, "port": port_arg}
-    print(args)
+  # get url parameters for HTML template
+  name_arg = request.args.get('name', 'mnist-classifier')
+  addr_arg = request.args.get('addr', 'localhost')
+  port_arg = request.args.get('port', '8080')
+  args = {"name": name_arg, "addr": addr_arg, "port": port_arg}
+  print(args)
 
-    output = None
-    connection = {"text": "", "success": False}
-    img_id = str(uuid.uuid4())
-    img_path = "static/tmp/" + img_id + ".png"
-    try:
-        # get a random test MNIST image
-        x, y, _ = random_mnist(img_path)
-        # get prediction from TensorFlow server
-        pred = get_prediction(x, server_host=addr_arg, server_port=int(port_arg),
-                              deployment_name=name_arg, timeout=10)
-        # if no exceptions thrown, server connection was a success
-        connection["text"] = "Connected to Seldon GRPC model serving service"
-        connection["success"] = True
-        # parse class confidence scores from server prediction
-        scores_dict = []
-        for i in range(0, 10):
-            scores_dict += [{"index": str(i), "val": pred[i]}]
-        output = {"truth": y,
-                  "img_path": img_path, "scores": scores_dict}
-    except Exception as e:
-        # server connection failed
-        connection["text"] = "Could Not Connect to Server: " + str(e)
-    # after 10 seconds, delete cached image file from server
-    t = Timer(10.0, remove_resource, [img_path])
-    t.start()
-    # render results using HTML template
-    return render_template('index.html', output=output,
-                           connection=connection, args=args)
+  output = None
+  connection = {"text": "", "success": False}
+  img_id = str(uuid.uuid4())
+  img_path = "static/tmp/" + img_id + ".png"
+  try:
+    # get a random test MNIST image
+    x, y, _ = random_mnist(img_path)
+    # get prediction from TensorFlow server
+    pred = get_prediction(x, server_host=addr_arg, server_port=int(port_arg),
+                          deployment_name=name_arg, timeout=10)
+    # if no exceptions thrown, server connection was a success
+    connection["text"] = "Connected to Seldon GRPC model serving service"
+    connection["success"] = True
+    # parse class confidence scores from server prediction
+    scores_dict = []
+    for i in range(0, 10):
+      scores_dict += [{"index": str(i), "val": pred[i]}]
+    output = {"truth": y,
+              "img_path": img_path, "scores": scores_dict}
+  except Exception as e:
+    # server connection failed
+    connection["text"] = "Could Not Connect to Server: " + str(e)
+  # after 10 seconds, delete cached image file from server
+  t = Timer(10.0, remove_resource, [img_path])
+  t.start()
+  # render results using HTML template
+  return render_template('index.html', output=output,
+                         connection=connection, args=args)
 
 
 def remove_resource(path):
-    """
-    attempt to delete file from path. Used to clean up MNIST testing images
+  """
+  attempt to delete file from path. Used to clean up MNIST testing images
 
-    :param path: the path of the file to delete
-    """
-    try:
-        os.remove(path)
-        print("removed " + path)
-    except OSError:
-        print("no file at " + path)
+  :param path: the path of the file to delete
+  """
+  try:
+    os.remove(path)
+    print("removed " + path)
+  except OSError:
+    print("no file at " + path)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0')
