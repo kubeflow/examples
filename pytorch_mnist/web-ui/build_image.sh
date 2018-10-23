07c5771c35347a2bf7011dff0578967513430d7b
David Sabater <davidsabater@google.com>
2018-10-23 17:31:43 +0100

    Update build_image.sh from CentralDashboard component

--- pytorch_mnist/web-ui/build_image.sh
+++ pytorch_mnist/web-ui/build_image.sh
@@ -3,26 +3,26 @@
 # A simple script to build the Docker images.
 # This is intended to be invoked as a step in Argo to build the docker image.
 #
-# build_image.sh ${DOCKERFILE} ${IMAGE} ${TAG} (optional | ${TEST_REGISTRY})
+# build_image.sh ${DOCKERFILE} ${IMAGE} ${TAG}
 set -ex
 
 DOCKERFILE=$1
-CONTEXT_DIR=$(dirname "$DOCKERFILE")
 IMAGE=$2
 TAG=$3
-
-BUILDER_IMG=gcr.io/kubeflow-images-public/bootstrapper-builder
-BUILDER_IMG_VERSION=$(head -1 ${CONTEXT_DIR}/glide.lock | cut -d ' ' -f 2)
+CONTEXT_DIR=$(dirname "$DOCKERFILE")
+PROJECT="${GCP_PROJECT}"
 
 # Wait for the Docker daemon to be available.
 until docker ps
 do sleep 3
 done
 
-# pull builder image from GCR or build it from local if required one doesn't exist.
-docker pull ${BUILDER_IMG}:${BUILDER_IMG_VERSION} || docker build -t ${BUILDER_IMG}:${BUILDER_IMG_VERSION} -f ${CONTEXT_DIR}/Dockerfile.Builder ${CONTEXT_DIR}
-
-python ${CONTEXT_DIR}/build.py --build_args=BUILDER_IMG=${BUILDER_IMG},BUILDER_IMG_VERSION=${BUILDER_IMG_VERSION} --image=${IMAGE}:${TAG} --test_registry=${4-}
-
 gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
-gcloud docker -- push "${IMAGE}:${TAG}"
+
+cd $CONTEXT_DIR
+
+echo "GCP Project: "$PROJECT
+
+echo "Building centraldashboard using gcloud build"
+gcloud builds submit --tag=${IMAGE}:${TAG} --project=${PROJECT} .
+echo "Finished building image"
