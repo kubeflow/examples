c676c8ca97f56756811a3dead93e6d8baa0004d7
IronPan <yangpa@google.com>
2018-11-19 23:07:36 -0800

    add code to push to git

---
+++ code_search/pipeline/push-to-git/push.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -ex
+
+
+usage()
+{
+    echo "usage: push.sh
+    [--gcs_file The GCS file to be pushed ]
+    [--git_repo The git repository to push the file to ]
+    [--user_email The email account to use for pushing the change ]
+    [-h help]"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+      --gcs_file )
+        shift
+        GCS_FILE=$1
+        ;;
+      --git_repo )
+        shift
+        GIT_REPO=$1
+        ;;
+      --user_email )
+        shift
+        USER_EMAIL=$1
+        ;;
+      -h | --help )
+        usage
+        exit
+        ;;
+      * )
+        usage
+        exit 1
+    esac
+    shift
+done
+
+git config --global user.email ${USER_EMAIL}
+git clone https://${GIT_TOKEN}@github.com/${GIT_REPO}.git repo
+cd repo && gsutil cp ${GCS_FILE} . && git add * && git commit -m "index update" && git push origin master
+
+
