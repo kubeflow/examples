88ed53f0745bc93b48238f6c1f2dd1a226bbdeae
David Sabater <davidsabater@google.com>
2019-02-03 16:24:24 +0000

    Commented out calls to tf-util until https://github.com/kubeflow/pytorch-operator/issues/108 is implemented

--- pytorch_mnist/testing/tfjob_test.py
+++ pytorch_mnist/testing/tfjob_test.py
@@ -1,12 +1,13 @@
-"""Test training using TFJob.
+"""Test training using PytorchJob.
 
 This file tests that we can submit the job from ksonnet
 and that the job runs to completion.
 
 It is an integration test as it depends on having access to
-a Kubeflow deployment to submit the TFJob to.
+a Kubeflow deployment to submit the PytorchJob to.
 
 Python Path Requirements:
+TODO(dsdinter) update reference to Pytorch-operator instead
   kubeflow/tf-operator/py - https://github.com/kubeflow/tf-operator
      * Provides utilities for testing TFJobs
   kubeflow/testing/py - https://github.com/kubeflow/testing/tree/master/py
@@ -35,7 +36,7 @@ from kubeflow.testing import ks_util
 from kubeflow.testing import test_util
 from kubeflow.testing import util
 
-class TFJobTest(test_util.TestCase):
+class PytorchJobTest(test_util.TestCase):
   def __init__(self, args):
     namespace, name, env = test_runner.parse_runtime_params(args)
     self.app_dir = args.app_dir
@@ -50,7 +51,7 @@ class TFJobTest(test_util.TestCase):
     self.namespace = namespace
     self.params = args.params
     self.ks_cmd = ks_util.get_ksonnet_cmd(self.app_dir)
-    super(TFJobTest, self).__init__(class_name="TFJobTest", name=name)
+    super(PytorchJobTest, self).__init__(class_name="PytorchJobTest", name=name)
 
   def test_train(self):
     # We repeat the test multiple times.
@@ -58,38 +59,43 @@ class TFJobTest(test_util.TestCase):
     # same name.
     api_client = k8s_client.ApiClient()
 
-    component = "train"
-    # Setup the ksonnet app
-    ks_util.setup_ks_app(self.app_dir, self.env, self.namespace, component,
-                         self.params)
+    # Apply the components
+    # TODO(dsdinter) we should test v1alpha2 in k3.5 cluster as well
+    for component in ["train_model_CPU_v1beta1", "train_model_GPU_v1beta1"]:
+      # Setup the ksonnet app
+      ks_util.setup_ks_app(self.app_dir, self.env, self.namespace, component,
+                           self.params)
 
+      util.run([self.ks_cmd, "apply", self.env, "-c", component],
+               cwd=self.app_dir)
 
-    # Create the TF job
-    util.run([self.ks_cmd, "apply", self.env, "-c", component],
-             cwd=self.app_dir)
-    logging.info("Created job %s in namespaces %s", self.name, self.namespace)
+      logging.info("Created PytorchJob %s in namespaces %s", self.name, self.namespace)
+
+    # We don't delete the resources. We depend on the namespace being
+    # garbage collected.
 
     # Wait for the job to complete.
-    logging.info("Waiting for job to finish.")
-    results = tf_job_client.wait_for_job(
-          api_client,
-          self.namespace,
-          self.name,
-          status_callback=tf_job_client.log_status)
-    logging.info("Final TFJob:\n %s", json.dumps(results, indent=2))
-
-    # Check for errors creating pods and services. Can potentially
-    # help debug failed test runs.
-    creation_failures = tf_job_client.get_creation_failures_from_tfjob(
-        api_client, self.namespace, results)
-    if creation_failures:
-      logging.warning(creation_failures)
-
-    if not tf_job_client.job_succeeded(results):
-      self.failure = "Job {0} in namespace {1} in status {2}".format(  # pylint: disable=attribute-defined-outside-init
-          self.name, self.namespace, results.get("status", {}))
-      logging.error(self.failure)
-      return
+    # logging.info("Waiting for job to finish.")
+    # TODO(dsdinter) update with Pytorch operator test util when implemented
+    # TODO(dsdinter) https://github.com/kubeflow/pytorch-operator/issues/108
+    # results = tf_job_client.wait_for_job(
+    #       api_client,
+    #       self.namespace,
+    #       self.name,
+    #       status_callback=tf_job_client.log_status)
+    # logging.info("Final PytorchJob:\n %s", json.dumps(results, indent=2))
+    #
+    # # Check for errors creating pods and services. Can potentially
+    # # help debug failed test runs.
+    # creation_failures = tf_job_client.get_creation_failures_from_tfjob(
+    #     api_client, self.namespace, results)
+    # if creation_failures:
+    #   logging.warning(creation_failures)
+    # if not tf_job_client.job_succeeded(results):
+    #  self.failure = "Job {0} in namespace {1} in status {2}".format(  # pylint: disable=attribute-defined-outside-init
+    #      self.name, self.namespace, results.get("status", {}))
+    #  logging.error(self.failure)
+    #  return
 
     # We don't delete the jobs. We rely on TTLSecondsAfterFinished
     # to delete old jobs. Leaving jobs around should make it
