ec5fe6d879438b191d7b97d5cdef832abd4bfffd
Jin Chi He <hejinchi@cn.ibm.com>
2019-05-17 16:18:48 +0800

    Examples for pipelines running on-premise

--- pipelines/mnist-pipelines/mnist_pipeline.py
+++ pipelines/mnist-pipelines/mnist_pipeline.py
@@ -20,7 +20,9 @@ Run this script to compile pipeline
 
 import kfp.dsl as dsl
 import kfp.gcp as gcp
+import kfp.onprem as onprem
 
+platform = 'GCP'
 
 @dsl.pipeline(
   name='MNIST',
@@ -29,7 +31,8 @@ import kfp.gcp as gcp
 def mnist_pipeline(model_export_dir='gs://your-bucket/export',
                    train_steps='200',
                    learning_rate='0.01',
-                   batch_size='100'):
+                   batch_size='100',
+                   pvc_name=''):
   """
   Pipeline with three stages:
     1. train an MNIST classifier
@@ -46,34 +49,54 @@ def mnist_pipeline(model_export_dir='gs://your-bucket/export',
           "--tf-batch-size", batch_size,
           "--tf-learning-rate", learning_rate
           ]
-  ).apply(gcp.use_gcp_secret('user-gcp-sa'))
+  )
+
+
+  serve_args = [
+      '--model-export-path', model_export_dir,
+      '--server-name', "mnist-service"
+  ]
+  if platform != 'GCP':
+    serve_args.extend([
+        '--cluster-name', "mnist-pipeline",
+        '--pvc-name', pvc_name
+    ])
 
   serve = dsl.ContainerOp(
       name='serve',
       image='gcr.io/ml-pipeline/ml-pipeline-kubeflow-deployer:'
             '7775692adf28d6f79098e76e839986c9ee55dd61',
-      arguments=[
-          '--model-export-path', model_export_dir,
-          '--server-name', "mnist-service"
-      ]
-  ).apply(gcp.use_gcp_secret('user-gcp-sa'))
+      arguments=serve_args
+  )
   serve.after(train)
 
-  web_ui = dsl.ContainerOp(
-      name='web-ui',
-      image='gcr.io/kubeflow-examples/mnist/deploy-service:latest',
-      arguments=[
+
+  webui_args = [
           '--image', 'gcr.io/kubeflow-examples/mnist/web-ui:'
                      'v20190304-v0.2-176-g15d997b-pipelines',
           '--name', 'web-ui',
           '--container-port', '5000',
           '--service-port', '80',
           '--service-type', "LoadBalancer"
-      ]
-  ).apply(gcp.use_gcp_secret('user-gcp-sa'))
+  ]
+  if platform != 'GCP':
+    webui_args.extend([
+      '--cluster-name', "mnist-pipeline"
+    ])
 
+  web_ui = dsl.ContainerOp(
+      name='web-ui',
+      image='gcr.io/kubeflow-examples/mnist/deploy-service:latest',
+      arguments=webui_args
+  )
   web_ui.after(serve)
 
+  steps = [train, serve, web_ui]
+  for step in steps:
+    if platform == 'GCP':
+      step.apply(gcp.use_gcp_secret('user-gcp-sa'))
+    else:
+      step.apply(onprem.mount_pvc(pvc_name, 'local-storage', '/mnt'))
 
 if __name__ == '__main__':
   import kfp.compiler as compiler
