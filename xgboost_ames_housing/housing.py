8c910c089a4f2ef8886616b9a0ad6275173bb5d7
Richard Liu <ricliu@google.com>
2019-05-31 18:16:46 -0700

    Fixes to XGBoost example

--- xgboost_ames_housing/housing.py
+++ xgboost_ames_housing/housing.py
@@ -21,7 +21,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import Imputer
 from xgboost import XGBRegressor
 
-def read_input(file_name, test_size=0.25):
+def read_input(file_name, test_size=0.25, shuffle=True):
   """Read input data and split it into train and test."""
   data = pd.read_csv(file_name[0])
   data.dropna(axis=0, subset=['SalePrice'], inplace=True)
@@ -32,7 +32,7 @@ def read_input(file_name, test_size=0.25):
   train_X, test_X, train_y, test_y = train_test_split(X.values,
                                                       y.values,
                                                       test_size=test_size,
-                                                      shuffle=False)
+                                                      shuffle=shuffle)
 
   imputer = Imputer()
   train_X = imputer.fit_transform(train_X)
@@ -45,14 +45,15 @@ def train_model(train_X,
                 test_X,
                 test_y,
                 n_estimators,
-                learning_rate):
+                learning_rate,
+                early_stopping_rounds):
   """Train the model using XGBRegressor."""
   model = XGBRegressor(n_estimators=n_estimators,
                       learning_rate=learning_rate)
 
   model.fit(train_X,
             train_y,
-            early_stopping_rounds=40,
+            early_stopping_rounds=early_stopping_rounds,
             eval_set=[(test_X, test_y)])
 
   logging.info("Best RMSE on eval: %.2f with %d rounds",
@@ -71,16 +72,20 @@ def save_model(model, model_file):
   logging.info("Model export success: %s", model_file)
 
 def main(args):
-  (train_X, train_y), (test_X, test_y) = read_input(args.train_input)
+  (train_X, train_y), (test_X, test_y) = read_input(args.train_input,
+                                                    args.test_size,
+                                                    args.shuffle)
   model = train_model(train_X,
                       train_y,
                       test_X,
                       test_y,
                       args.n_estimators,
-                      args.learning_rate)
+                      args.learning_rate,
+                      args.early_stopping_rounds)
 
   eval_model(model, test_X, test_y)
-  save_model(model, args.model_file)
+  if args.model_file:
+    save_model(model, args.model_file)
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
@@ -103,13 +108,19 @@ if __name__ == '__main__':
           )
   parser.add_argument(
           '--model-file',
-          help='Model file location for XGBoost',
-          required=True
+          help='Model file location for XGBoost'
           )
   parser.add_argument(
           '--test-size',
           help='Fraction of training data to be reserved for test',
+          type=float,
           default=0.25
+          )
+  parser.add_argument(
+          '--shuffle',
+          help='Whether to shuffle input before splitting',
+          type=bool,
+          default=True
           )
   parser.add_argument(
           '--early-stopping-rounds',
