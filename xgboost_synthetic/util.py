4707f2c27323f01730aae9139428007ee4c5d3d8
abcdefgs0324 <abcdefgs0324@gmail.com>
2019-07-16 09:14:09 -0700

    fix pylint.

--- xgboost_synthetic/util.py
+++ xgboost_synthetic/util.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import shutil
 import subprocess
@@ -8,40 +9,37 @@ from retrying import retry
 
 KFP_PACKAGE = 'https://storage.googleapis.com/ml-pipeline/release/0.1.20/kfp.tar.gz'
 def notebook_setup():
-    # Install the SDK
+  # Install the SDK
 
-    subprocess.check_call(["pip3", "install", KFP_PACKAGE, "--upgrade"])
+  subprocess.check_call(["pip3", "install", KFP_PACKAGE, "--upgrade"])
 
-    import logging
-    import os
+  logging.basicConfig(format='%(message)s')
+  logging.getLogger().setLevel(logging.INFO)
 
-    logging.basicConfig(format='%(message)s')
-    logging.getLogger().setLevel(logging.INFO)
-
-    subprocess.check_call(["gcloud", "auth", "configure-docker", "--quiet"])
-    subprocess.check_call(["gcloud", "auth", "activate-service-account",
-                           "--key-file=" + os.getenv("GOOGLE_APPLICATION_CREDENTIALS"),
-                           "--quiet"])
+  subprocess.check_call(["gcloud", "auth", "configure-docker", "--quiet"])
+  subprocess.check_call(["gcloud", "auth", "activate-service-account",
+                         "--key-file=" + os.getenv("GOOGLE_APPLICATION_CREDENTIALS"),
+                         "--quiet"])
 
 def copy_data_to_nfs(nfs_path, model_dir):
-    if not os.path.exists(nfs_path):
-        shutil.copytree("ames_dataset", nfs_path)
+  if not os.path.exists(nfs_path):
+    shutil.copytree("ames_dataset", nfs_path)
 
-    if not os.path.exists(model_dir):
-        os.makedirs(model_dir)
+  if not os.path.exists(model_dir):
+    os.makedirs(model_dir)
 
 @retry(wait_exponential_multiplier=1000, wait_exponential_max=5000,
        stop_max_delay=2*60*1000)
 def predict_nparray(url, data, feature_names=None):
-    pdata={
-        "data": {
-            "names":feature_names,
-            "tensor": {
-                "shape": np.asarray(data.shape).tolist(),
-                "values": data.flatten().tolist(),
-            },
-        }
-    }
-    serialized_data = json.dumps(pdata)
-    r = requests.post(url, data={'json':serialized_data}, timeout=5)
-    return r
+  pdata = {
+      "data": {
+          "names":feature_names,
+          "tensor": {
+              "shape": np.asarray(data.shape).tolist(),
+              "values": data.flatten().tolist(),
+          },
+      }
+  }
+  serialized_data = json.dumps(pdata)
+  r = requests.post(url, data={'json':serialized_data}, timeout=5)
+  return r
