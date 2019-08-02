e2d1c944476bed0e4b0fb09d8ef76ca13a9059d0
Rebecca McFadden <rem20806@tamu.edu>
2019-08-01 21:06:35 -0700

    Update inference.sh

--- pipelines/azurepipeline/code/deploy/inference.sh
+++ pipelines/azurepipeline/code/deploy/inference.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 echo "test the deployment with a burrito image"
-az ml service run -n fooddeployaci -d '{ "image": "https://www.exploreveg.org/files/2015/05/sofritas-burrito.jpeg" }' -w taco-workspace -g taco-rg
+az ml service run -n ${DEPLOYMENT_NAME} -d '{ "image": "https://www.exploreveg.org/files/2015/05/sofritas-burrito.jpeg" }' -w ${WORKSPACE} -g ${RESOURCE_GROUP}
 echo "test the deployment with a taco image"
-az ml service run -n fooddeployaci -d '{ "image": "https://c1.staticflickr.com/5/4022/4401140214_f489c708f0_b.jpg" }' -w taco-workspace -g taco-rg
+az ml service run -n ${DEPLOYMENT_NAME} -d '{ "image": "https://c1.staticflickr.com/5/4022/4401140214_f489c708f0_b.jpg" }' -w ${WORKSPACE} -g ${RESOURCE_GROUP}
