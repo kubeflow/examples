f65e03eaca7df1396dd9a326fa7bf89db5ae580a
Amy Unruh <amy@infosleuth.net>
2019-09-18 16:55:10 -0700

    update the codelab shortlink; revert to commenting out a problematic unit test

--- github_issue_summarization/testing/tfjob_test.py
+++ github_issue_summarization/testing/tfjob_test.py
@@ -23,17 +23,18 @@ Manually running the test
      --params=num_epochs=1,sample_size=10,...
 """
 
-import json
+# import json
 import logging
 import os
+# import unittest
 
-from kubernetes import client as k8s_client
-from kubeflow.tf_operator import tf_job_client #pylint: disable=no-name-in-module
+# from kubernetes import client as k8s_client
+# from kubeflow.tf_operator import tf_job_client #pylint: disable=no-name-in-module
 from kubeflow.tf_operator import test_runner #pylint: disable=no-name-in-module
 
 from kubeflow.testing import ks_util
 from kubeflow.testing import test_util
-from kubeflow.testing import util
+# from kubeflow.testing import util
 
 class TFJobTest(test_util.TestCase):
   def __init__(self, args):
@@ -52,49 +53,49 @@ class TFJobTest(test_util.TestCase):
     self.ks_cmd = ks_util.get_ksonnet_cmd(self.app_dir)
     super(TFJobTest, self).__init__(class_name="TFJobTest", name=name)
 
-  @test_util.expectedFailure
-  def test_train(self):
-    # We repeat the test multiple times.
-    # This ensures that if we delete the job we can create a new job with the
-    # same name.
-    api_client = k8s_client.ApiClient()
+  # @unittest.expectedFailure
+  # def test_train(self):
+  #   # We repeat the test multiple times.
+  #   # This ensures that if we delete the job we can create a new job with the
+  #   # same name.
+  #   api_client = k8s_client.ApiClient()
 
-    component = "tfjob"
-    # Setup the ksonnet app
-    ks_util.setup_ks_app(self.app_dir, self.env, self.namespace, component,
-                         self.params)
+  #   component = "tfjob"
+  #   # Setup the ksonnet app
+  #   ks_util.setup_ks_app(self.app_dir, self.env, self.namespace, component,
+  #                        self.params)
 
 
-    # Create the TF job
-    util.run([self.ks_cmd, "apply", self.env, "-c", component],
-             cwd=self.app_dir)
-    logging.info("Created job %s in namespaces %s", self.name, self.namespace)
+  #   # Create the TF job
+  #   util.run([self.ks_cmd, "apply", self.env, "-c", component],
+  #            cwd=self.app_dir)
+  #   logging.info("Created job %s in namespaces %s", self.name, self.namespace)
 
-    # Wait for the job to complete.
-    logging.info("Waiting for job to finish.")
-    results = tf_job_client.wait_for_job(
-          api_client,
-          self.namespace,
-          self.name,
-          status_callback=tf_job_client.log_status)
-    logging.info("Final TFJob:\n %s", json.dumps(results, indent=2))
+  #   # Wait for the job to complete.
+  #   logging.info("Waiting for job to finish.")
+  #   results = tf_job_client.wait_for_job(
+  #         api_client,
+  #         self.namespace,
+  #         self.name,
+  #         status_callback=tf_job_client.log_status)
+  #   logging.info("Final TFJob:\n %s", json.dumps(results, indent=2))
 
-    # Check for errors creating pods and services. Can potentially
-    # help debug failed test runs.
-    creation_failures = tf_job_client.get_creation_failures_from_tfjob(
-        api_client, self.namespace, results)
-    if creation_failures:
-      logging.warning(creation_failures)
+  #   # Check for errors creating pods and services. Can potentially
+  #   # help debug failed test runs.
+  #   creation_failures = tf_job_client.get_creation_failures_from_tfjob(
+  #       api_client, self.namespace, results)
+  #   if creation_failures:
+  #     logging.warning(creation_failures)
 
-    if not tf_job_client.job_succeeded(results):
-      self.failure = "Job {0} in namespace {1} in status {2}".format(  # pylint: disable=attribute-defined-outside-init
-          self.name, self.namespace, results.get("status", {}))
-      logging.error(self.failure)
-      return
+  #   if not tf_job_client.job_succeeded(results):
+  #     self.failure = "Job {0} in namespace {1} in status {2}".format(  # pylint: disable=attribute-defined-outside-init
+  #         self.name, self.namespace, results.get("status", {}))
+  #     logging.error(self.failure)
+  #     return
 
-    # We don't delete the jobs. We rely on TTLSecondsAfterFinished
-    # to delete old jobs. Leaving jobs around should make it
-    # easier to debug.
+  #   # We don't delete the jobs. We rely on TTLSecondsAfterFinished
+  #   # to delete old jobs. Leaving jobs around should make it
+  #   # easier to debug.
 
 if __name__ == "__main__":
   test_runner.main(module=__name__)
