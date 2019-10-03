ad55b0a246b01586e5e8f110b7d2dc5e5a5c3c1a
Amy <amy@infosleuth.net>
2019-10-03 09:12:09 -0700

    change gpu limit (#651)

--- github_issue_summarization/pipelines/example_pipelines/gh_summ.py
+++ github_issue_summarization/pipelines/example_pipelines/gh_summ.py
@@ -95,7 +95,7 @@ def gh_summ(  #pylint: disable=unused-argument
   train.after(copydata)
   log_model.after(train)
   serve.after(train)
-  train.set_gpu_limit(4)
+  train.set_gpu_limit(1)
   train.set_memory_limit('48G')
 
   with dsl.Condition(train.output == 'true'):
