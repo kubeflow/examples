7e28cd6b23f9b3d76c242cb16391ccbf01896516
Jeremy Lewi <jeremy+github@lewi.us>
2019-10-24 19:53:38 -0700

    Update xgboost_synthetic test infra; preliminary updates to work with 0.7.0 (#666)
    * Update xgboost_synthetic test infra to use pytest and pyfunc.
    
    * Related to #655 update xgboost_synthetic to use workload identity
    
    * Related to to #665 no signal about xgboost_synthetic
    
    * We need to update the xgboost_synthetic example to work with 0.7.0;
      e.g. workload identity
    
    * This PR focuses on updating the test infra and some preliminary
      updates the notebook
    
    * More fixes to the test and the notebook are probably needed in order
      to get it to actually pass
    
    * Update job spec for 0.7; remove the secret and set the default service
      account.
    
      * This is to make it work with workload identity
    
    * Instead of using kustomize to define the job to run the notebook we can just modify the YAML spec using python.
    * Use the python API for K8s to create the job rather than shelling out.
    
    * Notebook should do a 0.7 compatible check for credentials
    
      * We don't want to assume GOOGLE_APPLICATION_CREDENTIALS is set
        because we will be using workload identity.
    
    * Take in repos as an argument akin to what checkout_repos.sh requires
    
    * Convert xgboost_test.py to a pytest.
    
      * This allows us to mark it as expected to fail so we can start to get
        signal without blocking
    
      * We also need to emit junit files to show up in test grid.
    
    * Convert the jsonnet workflow for the E2E test to a python function to
      define the workflow.
    
      * Remove the old jsonnet workflow.
    
    * Address comments.
    
    * Fix issues with the notebook
    * Install pip packages in user space
      * 0.7.0 images are based on TF images and they have different permissions
    * Install a newer version of fairing sdk that works with workload identity
    
    * Split pip installing dependencies out of util.py and into notebook_setup.py
    
      * That's because util.py could depend on the packages being installed by
        notebook_setup.py
    
    * After pip installing the modules into user space; we need to add the local
      path for pip packages to the python otherwise we get import not found
      errors.

--- xgboost_synthetic/testing/xgboost_test.py
+++ xgboost_synthetic/testing/xgboost_test.py
@@ -1,100 +1,125 @@
-import argparse
+import datetime
 import logging
 import os
-import subprocess
 import time
+import uuid
+import yaml
 
+import pytest
+
+from kubernetes import client as k8s_client
+from kubernetes.client import rest
 from kubeflow.testing import util
 
-def create_job(args, app_dir): #pylint:disable=redefined-outer-name
+# TODO(jlewi): This test is currently failing because various things
+# need to be updated to work with 0.7.0. Until that's fixed we mark it
+# as expected to fail so we can begin to get signal.
+@pytest.mark.xfail
+def test_xgboost_synthetic(record_xml_attribute, name, namespace, cluster, # pylint: disable=too-many-branches,too-many-statements
+                           repos, image, app_dir):
   '''Generate Job and summit.'''
-  util.run(['gcloud', 'auth', 'activate-service-account',
-          "--key-file=/secret/gcp-credentials/key.json"], cwd=app_dir)
-  util.run(['gcloud', '--project=kubeflow-ci-deployment', 'container',
-          "clusters", "get-credentials", "--zone=us-east1-b", args.cluster], cwd=app_dir)
-
-  configmap = 'xgb-notebooks-tests'
-  util.run(['kustomize', 'edit', 'add', 'configmap', configmap,
-          '--from-literal=name=' + args.name], cwd=app_dir)
-  # For presubmit, set the checkout tag as HEAD:$(PULL_NUMBER), others set to PULL_BASE_SHA
-  if args.jobType == 'presubmit':
-    util.run(['kustomize', 'edit', 'add', 'configmap', configmap,
-            '--from-literal=checkTag=HEAD:' + args.pullNumber], cwd=app_dir)
-  else:
-    util.run(['kustomize', 'edit', 'add', 'configmap', configmap,
-            '--from-literal=checkTag=' + args.pullBaseSHA], cwd=app_dir)
-  util.run(['kustomize', 'edit', 'set', 'namespace', args.namespace], cwd=app_dir)
-  util.run(['kustomize', 'edit', 'set', 'image', 'execute-image=' + args.image], cwd=app_dir)
-  util.run(['kustomize', 'build', app_dir, '-o', 'generated.yaml'], cwd=app_dir)
-  util.run(['kubectl', 'apply', '-f', 'generated.yaml'], cwd=app_dir)
-  logging.info("Created job %s in namespaces %s", args.name, args.namespace)
-
-def get_pod_logs(name, namespace, app_dir): #pylint:disable=redefined-outer-name
-  '''Cannot get logs by k8s python api, using kubectl command to get logs.'''
-  logging.info("Getting pod %s logs...", name)
-  util.run(['kubectl', 'logs', name, '-n', namespace], cwd=app_dir)
-
-def check_job_status(namespace, app_dir): #pylint:disable=redefined-outer-name
-  '''Cannot get job by k8s python api, using kubectl command to check job status.'''
-  is_successed = False
-  pod_info, pod_name, pod_status = '', '', ''
-  for _ in range(0, 30):
-    time.sleep(60)
-    subCmd = "kubectl get pod -n " + namespace + " | grep -m1 xgboost-test"
-    pod_info = subprocess.run(subCmd,
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE,
-                              shell=True,
-                              universal_newlines=True)
-    if len(str(pod_info.stdout).split()) >= 2:
-      pod_name = str(pod_info.stdout).split()[0]
-      pod_status = str(pod_info.stdout).split()[2]
-
-    if pod_name:
-      if pod_status == "Pending":
-        logging.info("Pod %s is Pending.", pod_name)
-      elif pod_status == "Running":
-        logging.info("Pod %s is Running.", pod_name)
-      elif pod_status == "Completed":
-        logging.info("Pod %s is Completed.", pod_name)
-        get_pod_logs(pod_name, namespace, app_dir)
-        is_successed = True
-        break
-      elif pod_status == "Error":
-        get_pod_logs(pod_name, namespace, app_dir)
-        raise RuntimeError("Failed to execute notebook.")
-      else:
-        logging.warning("Pod %s status %s.", pod_name, pod_status)
-    else:
-      logging.warning("Cannot get the pod name, retry after 60 seconds.")
-
-  if not is_successed:
-    raise RuntimeError("Timeout to get the executing notebook pod after 30 munites.")
-
-
-if __name__ == "__main__":
-
-  logging.basicConfig(level=logging.INFO)
-
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-    "--name", help="deploy application name", type=str, required=True)
-  parser.add_argument(
-    "--namespace", help="The namespace for the application", type=str, required=True)
-  parser.add_argument(
-    "--image", help="Image name for the application", type=str, required=True)
-  parser.add_argument(
-    "--pullNumber", help="The PR number", type=str, required=True)
-  parser.add_argument(
-    "--pullBaseSHA", help="The pull base SHA", type=str, required=True)
-  parser.add_argument(
-    "--jobType", help="The job type such as presubmit or postsubmit", type=str, required=True)
-  parser.add_argument(
-    "--cluster", help="The cluster which the applition running in", type=str, required=True)
-
-  app_dir = os.path.dirname(__file__)
+  util.set_pytest_junit(record_xml_attribute, "test_xgboost_synthetic")
   app_dir = os.path.abspath(app_dir)
 
-  args = parser.parse_args()
-  create_job(args, app_dir)
-  check_job_status(args.namespace, app_dir)
+  if os.getenv("GOOGLE_APPLICATION_CREDENTIALS"):
+    util.run(['gcloud', 'auth', 'activate-service-account',
+            "--key-file=" + os.getenv("GOOGLE_APPLICATION_CREDENTIALS")],
+             cwd=app_dir)
+
+  # TODO(jlewi): We should just assume that kubeconfig has been set.
+  if cluster:
+    util.run(['gcloud', '--project=kubeflow-ci-deployment', 'container',
+              "clusters", "get-credentials", "--zone=us-east1-b", cluster],
+               cwd=app_dir)
+
+  with open("job.yaml") as hf:
+    job = yaml.load(hf)
+
+  # We need to checkout the correct version of the code
+  # in presubmits and postsubmits. We should check the environment variables
+  # for the prow environment variables to get the appropriate values.
+  # We should probably also only do that if the
+  # See
+  # https://github.com/kubernetes/test-infra/blob/45246b09ed105698aa8fb928b7736d14480def29/prow/jobs.md#job-environment-variables
+  if not repos:
+    version = "@HEAD"
+    if os.getenv("PULL_NUMBER"):
+      version = "@{0}:{1}".format(os.getenv("PULL_PULL_SHA"),
+                                  os.getenv("PULL_NUMBER"))
+
+    else:
+      if os.getenv("PULL_BASE_SHA"):
+        version = "@{0}".format(os.getenv("PULL_BASE_SHA"))
+
+    repos = "kubeflow/examples" + version
+
+  logging.info("Repos set to %s", repos)
+  job["spec"]["template"]["spec"]["initContainers"][0]["command"] = [
+    "/usr/local/bin/checkout_repos.sh",
+    "--repos=" + repos,
+    "--src_dir=/src",
+    "--depth=all",
+  ]
+  job["spec"]["template"]["spec"]["containers"][0]["image"] = image
+  util.load_kube_config(persist_config=False)
+
+  if name:
+    job["metadata"]["name"] = name
+  else:
+    job["metadata"]["name"] = ("xgboost-test-" +
+                               datetime.datetime.now().strftime("%H%M%S")
+                               + "-" + uuid.uuid4().hex[0:3])
+
+  job["metadata"]["namespace"] = namespace
+
+  # Create an API client object to talk to the K8s master.
+  api_client = k8s_client.ApiClient()
+  batch_api = k8s_client.BatchV1Api(api_client)
+
+  logging.info("Creating job:\n%s", yaml.dump(job))
+  batch_api.create_namespaced_job(job["metadata"]["namespace"], job)
+  logging.info("Created job %s in namespaces %s", name, namespace)
+
+  # Wait for job.
+  end_time = datetime.datetime.now() + datetime.timedelta(
+    minutes=15)
+
+  last_condition = None
+  while datetime.datetime.now() < end_time:
+    try:
+      job = batch_api.read_namespaced_job(name, namespace)
+    except rest.ApiException as e:
+      logging.error("There was a problem getting job %s.%s; %s",
+                    namespace, name, e)
+      time.sleep(10)
+      continue
+    # ready_replicas could be None
+    if not job.conditions:
+      logging.info("Job missing condition")
+      time.sleep(10)
+      continue
+
+    last_condition = job.conditions[-1]
+    if last_condition["type"] in ["Failed", "Complete"]:
+      break
+    logging.info("Waiting for job %s.%s", namespace, name)
+    time.sleep(10)
+
+  logging.info("Final Job spec:\n%s", yaml.safe_dump(job))
+  util.run(["kubectl", "describe", "job", "-n", namespace, name])
+
+  if not last_condition or last_condition["type"] not in ["Failed", "Complete"]:
+    logging.error("Timeout waiting for job %s.%s to finish.", namespace, name)
+    raise RuntimeError("Job {0}.{1} has last condition {2} which is not "
+                       "Complete".format(namespace, name,
+                       last_condition["type"] in ["Failed", "Complete"]))
+  assert last_condition["type"] == "Complete"
+
+if __name__ == "__main__":
+  logging.basicConfig(level=logging.INFO,
+                      format=('%(levelname)s|%(asctime)s'
+                              '|%(pathname)s|%(lineno)d| %(message)s'),
+                      datefmt='%Y-%m-%dT%H:%M:%S',
+                      )
+  logging.getLogger().setLevel(logging.INFO)
+  pytest.main()
--- xgboost_synthetic/util.py
+++ xgboost_synthetic/util.py
@@ -1,26 +1,10 @@
-import logging
 import os
-import shutil
-import subprocess
 import json
+import shutil
 import requests
+
 from retrying import retry
 import numpy as np
-
-KFP_PACKAGE = 'https://storage.googleapis.com/ml-pipeline/release/0.1.20/kfp.tar.gz'
-def notebook_setup():
-  # Install the SDK
-
-  subprocess.check_call(["pip3", "install", "-r", "requirements.txt"])
-  subprocess.check_call(["pip3", "install", KFP_PACKAGE, "--upgrade"])
-
-  logging.basicConfig(format='%(message)s')
-  logging.getLogger().setLevel(logging.INFO)
-
-  subprocess.check_call(["gcloud", "auth", "configure-docker", "--quiet"])
-  subprocess.check_call(["gcloud", "auth", "activate-service-account",
-                         "--key-file=" + os.getenv("GOOGLE_APPLICATION_CREDENTIALS"),
-                         "--quiet"])
 
 def copy_data_to_nfs(nfs_path, model_dir):
   if not os.path.exists(nfs_path):
